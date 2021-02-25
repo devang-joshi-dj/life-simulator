cb6c8c6b91830d61cad6b7749a7a7907af7b0d43
Devang Joshi <devangjoshi11@gmail.com>
2021-02-25 23:53:27 +0530

    Added Basic Structure, Style and Navigation

---
+++ js/elements.js
@@ -0,0 +1,34 @@
+export const containerElement = document.querySelector('.container') // Main Container
+export const statisticsElement = document.querySelector('.statistics') // Statistic
+export const placesHeaderElement = document.querySelector('.header') // Header
+export const placesFooterElement = document.querySelector('.footer') // Footer
+export const mainElement = document.querySelector('.main') // Main Content Container
+
+// Statistics Element Start
+// Statistics Element End
+
+// Main Content Container Elements Start
+export const homeContentElement = document.querySelector('.homeContent')
+export const universityContentElement = document.querySelector('.universityContent')
+export const jobContentElement = document.querySelector('.jobContent')
+export const bankContentElement = document.querySelector('.bankContent')
+export const gymContentElement = document.querySelector('.gymContent')
+export const mallContentElement = document.querySelector('.mallContent')
+export const worshipContentElement = document.querySelector('.worshipContent')
+export const clubContentElement = document.querySelector('.clubContent')
+export const restaurantContentElement = document.querySelector('.restaurantContent')
+export const spaContentElement = document.querySelector('.spaContent')
+// Main Content Container Elements End
+
+// Buttons of Header And Footer Start
+export const homeElement = document.querySelector('.home')
+export const universityElement = document.querySelector('.university')
+export const jobElement = document.querySelector('.job')
+export const bankElement = document.querySelector('.bank')
+export const gymElement = document.querySelector('.gym')
+export const mallElement = document.querySelector('.mall')
+export const worshipElement = document.querySelector('.worship')
+export const clubElement = document.querySelector('.club')
+export const restaurantElement = document.querySelector('.restaurant')
+export const spa = document.querySelector('.spa')
+// Buttons of Header And Footer End
