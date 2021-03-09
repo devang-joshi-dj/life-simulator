68eba98391147476ee8e09c93ef3d8f7d883f361
Devang Joshi <devangjoshi11@gmail.com>
2021-03-09 17:11:05 +0530

    Added Decision Buttons

--- js/elements.js
+++ js/elements.js
@@ -32,3 +32,73 @@ export const clubElement = document.querySelector('.club')
 export const restaurantElement = document.querySelector('.restaurant')
 export const spaElement = document.querySelector('.spa')
 // Buttons of Header And Footer End
+
+//buttons of Home
+export const homeMeal = document.querySelector('#homeMeal')
+export const homeRest = document.querySelector('#homeRest')
+export const homeMeetFamily = document.querySelector('#homeMeetFamily')
+export const homeCleanRoom = document.querySelector('#homeCleanRoom')
+
+// buttons of University
+export const universityAdmission = document.querySelector('#universityAdmission')
+export const universityPayFees = document.querySelector('#universityPayFees')
+export const universityAttendClasses = document.querySelector('#universityAttendClasses')
+export const universityGroupStudy = document.querySelector('#universityGroupStudy')
+export const universityClubMeeting = document.querySelector('#universityClubMeeting')
+export const universityEatCanteen = document.querySelector('#universityEatCanteen')
+export const universityPlayGames = document.querySelector('#universityPlayGames')
+export const universityStudyLibrary = document.querySelector('#universityStudyLibrary')
+
+// buttons of Job
+export const jobWorkOneHour = document.querySelector('#jobWorkOneHour')
+export const jobWorkThreeHours = document.querySelector('#jobWorkThreeHours')
+export const jobWorkFiveHours = document.querySelector('#jobWorkFiveHours')
+export const jobWorkNineHours = document.querySelector('#jobWorkNineHours')
+export const jobTakeBreak = document.querySelector('#jobTakeBreak')
+export const jobAttendMeeting = document.querySelector('#jobAttendMeeting')
+export const jobApplyPromotion = document.querySelector('#jobApplyPromotion')
+
+// buttons of Bank
+export const bankOpenAccount = document.querySelector('#bankOpenAccount')
+export const bankDepositMoney = document.querySelector('#bankDepositMoney')
+export const bankWithdrawMoney = document.querySelector('#bankWithdrawMoney')
+export const bankTakeLoan = document.querySelector('#bankTakeLoan')
+export const bankPayBills = document.querySelector('#bankPayBills')
+export const bankTalkTeller = document.querySelector('#bankTalkTeller')
+
+// buttons of Gym
+export const gymTakeAdmission = document.querySelector('#gymTakeAdmission')
+export const gymExerciseOneHour = document.querySelector('#gymExerciseOneHour')
+export const gymExerciseTwoHours = document.querySelector('#gymExerciseTwoHours')
+export const gymExerciseThreeHours = document.querySelector('#gymExerciseThreeHours')
+export const gymDietPlan = document.querySelector('#gymDietPlan')
+export const gymTalkTrainer = document.querySelector('#gymTalkTrainer')
+
+// buttons of Mall
+export const mallShopping = document.querySelector('#mallShopping')
+export const mallWatchMovies = document.querySelector('#mallWatchMovies')
+export const mallGamingZone = document.querySelector('#mallGamingZone')
+export const mallFoodCourt = document.querySelector('#mallFoodCourt')
+
+// buttons of Worship
+export const worshipPray = document.querySelector('#worshipPray')
+export const worshipServe = document.querySelector('#worshipServe')
+export const worshipCharity = document.querySelector('#worshipCharity')
+
+// buttons of Club
+export const clubDance = document.querySelector('#clubDance')
+export const clubDrinkEat = document.querySelector('#clubDrinkEat')
+export const clubMeetPeople = document.querySelector('#clubMeetPeople')
+
+// buttons of Restaurant
+export const restaurantEatFastFood = document.querySelector('#restaurantEatFastFood')
+export const restaurantEatSeaFood = document.querySelector('#restaurantEatSeaFood')
+export const restaurantEatSalad = document.querySelector('#restaurantEatSalad')
+export const restaurantEatSouthIndian = document.querySelector('#restaurantEatSouthIndian')
+export const restaurantEatNorthIndian = document.querySelector('#restaurantEatNorthIndian')
+export const restaurantEatItalianFood = document.querySelector('#restaurantEatItalianFood')
+export const restaurantEatChineseFood = document.querySelector('#restaurantEatChineseFood')
+
+// buttons of Spa
+export const spaMassage = document.querySelector('#spaMassage')
+export const spaManicurePedicure = document.querySelector('#spaManicurePedicure')
