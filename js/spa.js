3b08d24bc36b31ba9032d8647df370a6a365710a
Priya Gupta <58894967+pg00019@users.noreply.github.com>
2021-04-08 17:06:59 +0530

    added club.js , restaurant.js , spa.js

---
+++ js/spa.js
@@ -0,0 +1,51 @@
+import * as elements from './elements.js'
+document.addEventListener("DOMContentLoaded", () => {
+
+    elements.spaMassage.addEventListener("click", Massage);
+    elements.spaManicurePedicure.addEventListener("click", ManicurePedicure);
+
+    var update_health = 0;
+    var update_happiness = 0;
+    var update_cash = 0;
+
+    function Massage() {
+        //health
+        var healthVal = elements.healthValue.innerText;
+        update_health = Number(healthVal);
+        update_health = update_health + update_health * (10 / 100);
+        elements.healthValue.innerHTML = update_health.toFixed(2);
+
+        //happiness
+        var happinessVal = elements.happinessValue.innerText;
+        update_happiness = Number(happinessVal);
+        update_happiness = update_happiness + update_happiness * (15 / 100);
+        elements.happinessValue.innerHTML = update_happiness.toFixed(2);
+
+        //cash
+        var cashVal = elements.cashValue.innerText;
+        update_cash = Number(cashVal);
+        update_cash = update_cash - update_cash * (20 / 100);
+        elements.cashValue.innerHTML = update_cash.toFixed(2);
+        elements.cashValue.innerHTML = update_cash + 2;
+    }
+    function ManicurePedicure() {
+        //health
+        var healthVal = elements.healthValue.innerText;
+        update_health = Number(healthVal);
+        update_health = update_health + update_health * (10 / 100);
+        elements.healthValue.innerHTML = update_health.toFixed(2);
+
+        //happiness
+        var happinessVal = elements.happinessValue.innerText;
+        update_happiness = Number(happinessVal);
+        update_happiness = update_happiness + update_happiness * (15 / 100);
+        elements.happinessValue.innerHTML = update_happiness.toFixed(2);
+
+        //cash
+        var cashVal = elements.cashValue.innerText;
+        update_cash = Number(cashVal);
+        update_cash = update_cash - update_cash * (20 / 100);
+        elements.cashValue.innerHTML = update_cash.toFixed(2);
+        elements.cashValue.innerHTML = update_cash + 2;
+    }
+})
