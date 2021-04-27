4a6eff2fd1839f46b3d810ec6d404317b72acc92
Priya Gupta <58894967+pg00019@users.noreply.github.com>
2021-04-27 23:09:09 +0530

    Update home.js

--- js/home.js
+++ js/home.js
@@ -4,9 +4,9 @@ import * as config from './config.js'
 document.addEventListener("DOMContentLoaded", () => {
 
 	const homeMeal = () => {
-		config.updateValue(config.health, +2)
-		config.updateValue(config.hunger, +2)
-		config.updateValue(config.happiness, +2)
+		config.updateValue(config.health, +5)
+		config.updateValue(config.hunger, +5)
+		config.updateValue(config.happiness, +4)
 		config.updateTime(0, 20)
 	}
 
@@ -27,6 +27,10 @@ document.addEventListener("DOMContentLoaded", () => {
 		config.updateValue(config.happiness, +2)
 		config.updateTime(1);
 	}
+	
+	document.addEventListener('click', () => {
+		config.disableElement(elements.homeMeetfamily, 0, 7)
+		config.disableElement(elements.homeCleanRoom,0,7)
 
 	elements.homeMeal.addEventListener("click", homeMeal)
 	elements.homeRest.addEventListener("click", homeRest)
