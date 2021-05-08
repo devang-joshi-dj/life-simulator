d44640a1e7df20507c17c7248114e5b7171ed644
Devang Joshi <devangjoshi11@gmail.com>
2021-05-08 15:33:13 +0530

    Added Game Over Functionality

--- js/config.js
+++ js/config.js
@@ -1,34 +1,33 @@
 import * as elements from './elements.js'
 
 document.addEventListener("DOMContentLoaded", () => {
-
-	const initialHealth = 50
-	const initialHunger = 50
-	const initialHappiness = 50
-	const initialCash = 1000
-	const initialIntelligence = 0
-	const initialStrength = 0
-	const initialSpirituality = 0
-	const initialHour = 7
-	const initialMinute = 0
-	const initialFoodAndGrocery = 10
-
-	const setValues = () => {
-		// function for setting all values
-		setValue(health, initialHealth)
-		setValue(hunger, initialHunger)
-		setValue(happiness, initialHappiness)
-		setValue(cash, initialCash)
-		setValue(intelligence, initialIntelligence)
-		setValue(strength, initialStrength)
-		setValue(spirituality, initialSpirituality)
-		setValue(hour, initialHour)
-		setValue(minute, initialMinute)
-		setValue(foodAndGrocery, initialFoodAndGrocery)
-	}
-
 	setValues()
 })
+
+const initialHealth = 50
+const initialHunger = 50
+const initialHappiness = 50
+const initialCash = 1000
+const initialIntelligence = 0
+const initialStrength = 0
+const initialSpirituality = 0
+const initialHour = 7
+const initialMinute = 0
+const initialFoodAndGrocery = 10
+
+const setValues = () => {
+	// function for setting all values
+	setValue(health, initialHealth)
+	setValue(hunger, initialHunger)
+	setValue(happiness, initialHappiness)
+	setValue(cash, initialCash)
+	setValue(intelligence, initialIntelligence)
+	setValue(strength, initialStrength)
+	setValue(spirituality, initialSpirituality)
+	setValue(hour, initialHour)
+	setValue(minute, initialMinute)
+	setValue(foodAndGrocery, initialFoodAndGrocery)
+}
 
 export const setValue = (element, value) => {
 	// function for setting a value to an element
@@ -38,6 +37,10 @@ export const setValue = (element, value) => {
 export let health = elements.healthValue
 export let hunger = elements.hungerValue
 export let happiness = elements.happinessValue
+
+export let hungerZero = 0;
+export let happinessZero = 0;
+export let healthZero = 0;
 
 export let intelligence = elements.intelligenceValue
 export let strength = elements.strengthValue
@@ -63,6 +66,57 @@ export const updateValue = (element, value) => {
 
 	if (element.innerHTML < 0 && (element == health || element == hunger || element == happiness || element == cash)) {
 		element.innerHTML = 0
+	}
+
+	if (element == hunger) {
+		if (element.innerHTML == 100) {
+			hungerZero++;
+			console.log(hungerZero)
+			if (hungerZero > 3) {
+				alert('You are dead due to hunger. GAME OVER')
+				setValues()
+			}
+			else {
+				alert('Please eat something')
+			}
+		}
+		else {
+			hungerZero = 0
+		}
+	}
+
+	if (element == happiness) {
+		if (element.innerHTML == 0) {
+			happinessZero++;
+			console.log(happinessZero)
+			if (happinessZero > 3) {
+				alert('You are dead due to depression. GAME OVER')
+				setValues()
+			}
+			else {
+				alert('You are depressed')
+			}
+		}
+		else {
+			happinessZero = 0
+		}
+	}
+
+	if (element == health) {
+		if (element.innerHTML == 0) {
+			healthZero++;
+			console.log(healthZero)
+			if (healthZero > 3) {
+				alert('You are dead due to health. GAME OVER')
+				setValues()
+			}
+			else {
+				alert('Your health is very low')
+			}
+		}
+		else {
+			healthZero = 0
+		}
 	}
 }
 
